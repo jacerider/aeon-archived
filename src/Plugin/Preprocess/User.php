d2b8e791b070a080e292951eba07586744d224c8
Cyle Carlson <jacerider@gmail.com>
2017-01-26 09:32:18 -0600

    Add user template and preprocessing.

---
+++ src/Plugin/Preprocess/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\aeon\Plugin\Preprocess;
+
+use Drupal\aeon\Utility\Variables;
+
+/**
+ * Pre-processes variables for the "user" theme hook.
+ *
+ * @ingroup plugins_preprocess
+ *
+ * @AeonPreprocess("user")
+ */
+class User extends PreprocessBase implements PreprocessInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preprocessVariables(Variables $variables) {
+    // Add view mode to user.html.twig.
+    $variables['view_mode'] = $variables['elements']['#view_mode'];
+  }
+
+}
