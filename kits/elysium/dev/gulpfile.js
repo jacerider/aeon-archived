7c3c89493daa641b0a23c0bde7203987cbaad79a
JaceRider <jacerider@gmail.com>
2017-07-24 11:35:48 -0500

    Remove once as it breaks reloading.

--- kits/elysium/dev/gulpfile.js
+++ kits/elysium/dev/gulpfile.js
@@ -71,6 +71,7 @@ gulp.task('css', function () {
     })
     .pipe(config.browserSync.enabled ? browserSync.reload({
       stream: true,
+      // once: true,
       match: '**/*.css'
     }) : gutil.noop());
 });
@@ -87,6 +88,7 @@ gulp.task('js', function () {
     .pipe(gulp.dest(config.js.dest))
     .pipe(config.browserSync.enabled ? browserSync.reload({
       stream: true,
+      // once: true,
       match: '**/*.js'
     }) : gutil.noop());
 });
