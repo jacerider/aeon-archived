9c21228ec5f3f3fb17465e9b14946b357708e336
JaceRider <jacerider@gmail.com>
2021-11-16 12:34:09 -0600

    Remove unused code.

--- src/Plugin/Preprocess/Html.php
+++ src/Plugin/Preprocess/Html.php
@@ -17,15 +17,6 @@ class Html extends PreprocessBase {
    * {@inheritdoc}
    */
   public function preprocessVariables(Variables $variables) {
-    $route_name = \Drupal::routeMatch()->getRouteName();
-    switch ($route_name) {
-      case 'user.login':
-      case 'user.register':
-      case 'user.pass':
-      case 'user.reset.form':
-        $suggestions[] = 'page__user__auth';
-        break;
-    }
   }
 
 }
